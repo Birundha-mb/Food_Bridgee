ab6bc13848a81fb3fcbdb82741436109e7fd2b63
Birundha-mb <mbirundha80@gmail.com>
2026-09-05 14:48:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.APPLICATION_URL || "http://localhost:5173",
+    origin: process.env.APPLICATION_URL || "https://frontend-7ucg1hfuq-birundha1.vercel.app/",
   })
 );
 
